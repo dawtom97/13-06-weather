d2a765ad3802c165a6101886ec85e12bde5e098f
dawtom97 <dawtom97@gmail.com>
2026-06-14 15:30:16 +0200

    streamlity dashboard

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 from services.openweather_api import get_weather
 from services.excel_files import save_file, read_file
+from services.dashboard import render
+
+render()
 
 # data_from_file = read_file()
 # print(data_from_file)
 
-while True:
-    weather_record = get_weather()
-    save_file([weather_record])
-    print("Pobrano informacje")
-    time.sleep(10)
+# while True:
+#     weather_record = get_weather()
+#     save_file([weather_record])
+#     print("Pobrano informacje")
+#     time.sleep(10)
 
